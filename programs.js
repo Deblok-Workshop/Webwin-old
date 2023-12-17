53cc40c793bd4640688ddf1bafe157ae2ea788f6
uhidontkno <bjaaa2gmlmain@gmail.com>
2023-12-16 20:38:18 -0500

    TurboWarp and fix startmenu misalignment

--- programs.js
+++ programs.js
@@ -16,3 +16,9 @@ function rh() {
 function db() {
     createWindow({"title":"Google Chrome","content":"<iframe src='runtimes/runner_db_dll.html' style='width:99%;height:99%;min-width:480px;min-height:240px;top:0;left:0;'></iframe>","icon":"icons/chrome.png"})
 }
+function tw() {
+    createWindow({"title":"TurboWarp","content":"<iframe src='runtimes/runner_tw.html' style='width:99%;height:99%;min-width:480px;min-height:240px;top:0;left:0;'></iframe>","icon":"icons/tw.png"})
+}
+function twc() {
+    createWindow({"title":"TurboWarp","content":"<iframe src='runtimes/runner_twc.html' style='width:99%;height:99%;min-width:480px;min-height:240px;top:0;left:0;'></iframe>","icon":"icons/tw.png"})
+}
